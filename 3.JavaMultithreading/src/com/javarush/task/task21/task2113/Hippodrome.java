8e87959de52c1cdbf4a102713acb7c4cb1a69b50
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-07-04 22:47:45 +0300

    Solution

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,15 +1,55 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses;
+    public static Hippodrome game;
 
     public List<Horse> getHorses() {
         return horses;
     }
 
-    public static void main(String[] args) {
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
 
+    public void move(){
+        for(Horse horse: horses){
+            horse.move();
+        }
+    }
+
+    public void print(){
+        for(Horse horse: horses){
+            horse.print();
+        }
+
+        for (int i = 0; i < 10; i++){
+            System.out.println();
+        }
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 1; i <= 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Horse horse1 = new Horse("Willy", 3, 0);
+        Horse horse2 = new Horse("Lucha", 3, 0);
+        Horse horse3 = new Horse("Nusha", 3, 0);
+
+        List<Horse> list= new ArrayList<>();
+        list.add(horse1);
+        list.add(horse2);
+        list.add(horse3);
+
+        game = new Hippodrome(list);
+        game.run();
     }
 }
