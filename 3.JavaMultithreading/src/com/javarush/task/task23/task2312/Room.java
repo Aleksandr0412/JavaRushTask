293cda2a39d63deef34a66e18e3a6d7b916a9421
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-07-16 02:00:25 +0300

    Solution

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -5,15 +5,12 @@ public class Room {
     private int height;
     private Snake snake;
     private Mouse mouse;
+    public static Room game;
 
     public Room(int width, int height, Snake snake) {
         this.width = width;
         this.height = height;
         this.snake = snake;
-    }
-
-    public static void main(String[] args) {
-
     }
 
     public int getWidth() {
@@ -46,5 +43,19 @@ public class Room {
 
     public void setMouse(Mouse mouse) {
         this.mouse = mouse;
+    }
+
+    public void run(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public static void main(String[] args) {
+        Snake snake = new Snake(10,10);
+        snake.setDirection(SnakeDirection.DOWN);
+        game = new Room(50, 50 , snake);
     }
 }
