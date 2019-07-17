d88cc13a5e78573b5c27f0d10d14e9e590e2ab7a
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-07-18 02:57:45 +0300

    Solution

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -38,4 +38,25 @@ public class Canvas {
     public void draw() {
 
     }
+
+    void setPoint(double x, double y, char c) {
+        int intX = (int) Math.round(x);
+        int intY = (int) Math.round(y);
+        if (intX >= 0 && intY >= 0 && intX < matrix[0].length && intY < matrix.length) {
+            matrix[intY][intX] = c;
+        } else return;
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    this.setPoint(x + j, y + i, c);
+                }
+            }
+
+        }
+    }
+
+
 }
