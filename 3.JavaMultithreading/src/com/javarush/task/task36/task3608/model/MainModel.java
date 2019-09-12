eea4f56578833c56355afa549697a3edf3af572f
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-09-12 19:47:36 +0300

    Solution

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+    private List<User> list = new ArrayList<>(2);
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+
+    }
+
+}
