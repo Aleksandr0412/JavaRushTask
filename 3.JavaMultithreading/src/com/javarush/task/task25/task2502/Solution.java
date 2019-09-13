57d58828787e7596b4c0e1ef021439f145aa10d8
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-09-13 13:26:23 +0300

    Solution

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception {
+            wheels = new ArrayList<>();
+            int count = 0;
+            for (String s : this.loadWheelNamesFromDB()) {
+                count++;
+                wheels.add(Wheel.valueOf(s));
+            }
+            if (count != 4) throw new Exception();
+            //init wheels here
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+}
