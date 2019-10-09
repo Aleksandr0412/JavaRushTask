1ede10b2b25c38865866c558a3e91b80beda3892
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-10-10 00:07:35 +0300

    Solution 8

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
@@ -8,32 +8,32 @@ public class StudentsDataBase {
 
     public static void addInfoAboutStudent(Student st) {
         students.add(st);
-        printInfoAboutStudent( st);
+        printInfoAboutStudent(st);
 
     }
 
-    public static void printInfoAboutStudent( Student student) {
+    public static void printInfoAboutStudent(Student student) {
         System.out.println("Имя: " + student.getName() + " Возраст: " + student.getAge());
     }
 
-    public static void removeStudent(int index) throws IndexOutOfBoundsException {
-        students.remove(index);
+    public static void removeStudent(int index) {
+        if (students.size() >= index && index >= 0)
+            students.remove(students.get(index));
     }
 
     public static void findDimaOrSasha() {
-        boolean found = false;
         for (int i = 0; i < students.size(); i++) {
-            if (!found) {
-                if (students.get(i).getName().equals("Dima")) {
-                    System.out.println("Студент Dima есть в базе.");
-                    found = true;
-                }
 
-                if (students.get(i).getName().equals("Sasha")) {
-                    System.out.println("Студент Sasha есть в базе.");
-                    found = true;
-                }
+            if (students.get(i).getName().equals("Dima")) {
+                System.out.println("Студент Dima есть в базе.");
+                break;
             }
+
+            if (students.get(i).getName().equals("Sasha")) {
+                System.out.println("Студент Sasha есть в базе.");
+                break;
+            }
+
         }
     }
 }
