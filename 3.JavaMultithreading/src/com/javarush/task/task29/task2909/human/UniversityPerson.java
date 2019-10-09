7ddedfd4d361788e9e105cd0e306312c708c0df6
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-10-09 19:58:17 +0300

    Solution 4

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/UniversityPerson.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task29.task2909.human;
+
+public class UniversityPerson extends Human{
+    protected String name;
+    protected int age;
+    private University university;
+
+    public UniversityPerson(String name, int age) {
+        super(name, age);
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
