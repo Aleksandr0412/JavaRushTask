dade47f2cb715497ee3e0ce87fb439b0e7be72f9
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-10-10 02:45:09 +0300

    Solution 14

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,22 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
+    private Address address = new Address();
 
-    private String country;
-    private String city;
-    private House house;
+    public String getBoss() {
+        Work work = this.getWork();
+        return work.getBoss();
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
 
     private Work work;
 
@@ -42,23 +54,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -73,6 +85,7 @@ public class User {
         System.out.println("Имя: " + getName());
         System.out.println("Фамилия: " + getSurname());
     }
+
     public void printAdditionalInfo() {
         if (getAge() < 16)
             System.out.println("Пользователь моложе 16 лет");
