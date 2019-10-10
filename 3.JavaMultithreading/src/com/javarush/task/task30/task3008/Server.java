bdcbac5d839d7262b7d11f32c3ecb4f6c969e54a
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2019-10-10 21:23:12 +0300

    Solution 6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,34 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int serverPort = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+            while (true) {
+                //Слушаем
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                //запускаем handler
+                handler.start();
+            }
+        }
+        catch (Exception e) {
+            ConsoleHelper.writeMessage("Сообщение не отправлено");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
